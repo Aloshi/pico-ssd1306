a31dbbde45d8f83f77232b7a5f401246079f5587
Alec Lofquist <alces14@gmail.com>
2023-04-10 14:08:48 -0500

    Better vscode config using picotool, don't hardcode pico sdk path, support osx

--- upload-pico.sh
+++ upload-pico.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 UF2_PATH=${1:-build/*.uf2}
-PICO_DIR=${2:-/media/`whoami`/RPI-RP2}
+
+if [[ $(uname) == "Darwin" ]]; then
+  PICO_DIR=${2:-/Volumes/RPI-RP2}
+else
+  PICO_DIR=${2:-/media/`whoami`/RPI-RP2}
+fi
 
 while [ ! -d "$PICO_DIR" ]; do
 	echo "Waiting for $PICO_DIR to exist..."
 	sleep 1;
 done
+
 cp $UF2_PATH "$PICO_DIR"
